61a8c65defc3ca038a2965a88d7630e25f7806f4
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2022-07-03 16:37:09 +0200

    Add unit tests project.

---
+++ Tests/Application/Order/CreateOrderCommandHandlerUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tests.Application.Order
+{
+    public class CreateOrderCommandHandlerUnitTests
+    {
+        [Fact]
+        public void AssertTrue()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
